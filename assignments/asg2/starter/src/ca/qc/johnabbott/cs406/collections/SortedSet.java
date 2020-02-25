870204ebceec359ccaf9c80ba91d4dfd6ab77bda
Galen Hu <1858105@johnabbottcollege.net>
2020-02-24 20:52:59 -0500

    Comment Added and ToString Modify

--- assignments/asg2/starter/src/ca/qc/johnabbott/cs406/collections/SortedSet.java
+++ assignments/asg2/starter/src/ca/qc/johnabbott/cs406/collections/SortedSet.java
@@ -11,8 +11,8 @@ public class SortedSet<T extends Comparable<T>> implements Set<T> {
 
     private T[] elements;
     private int size;
-    private int front;
-    private boolean modified;
+    private int front;          //use for traversal
+    private boolean modified;   //identify if array has been modified in traversal
 
     public SortedSet() {
         this(DEFAULT_CAPACITY);
@@ -25,24 +25,24 @@ public class SortedSet<T extends Comparable<T>> implements Set<T> {
         this.modified = false;
     }
 
+    //Look if the element is contain in the set
     @Override
     public boolean contains(T elem) {
         // elements is sorted, so we can binary search for the element.
         return Arrays.binarySearch(elements, 0, size, elem) >= 0;
     }
 
+    // all the element on the right side is in the set
     @Override
     public boolean containsAll(Set<T> rhs) {
-        System.out.println(rhs.toString());
-        System.out.println(this.toString());
-        if (rhs.isEmpty() && !this.isEmpty())
+        if (rhs.isEmpty() && !this.isEmpty())   //rhs is empty
             return true;
-        if(rhs.isEmpty() && this.isEmpty())
+        if(rhs.isEmpty() && this.isEmpty())     //both sets are empty
             return true;
-        if(this.isEmpty() && !rhs.isEmpty())
+        if(this.isEmpty() && !rhs.isEmpty())    //current set is empty, but rhs set has element
             return false;
 
-        rhs.reset();
+        rhs.reset();                            //reset traversal pointer
         for (int i = 0; i < rhs.size(); i++){
             if(rhs.hasNext()){
                 T tmp = rhs.next();
@@ -53,14 +53,15 @@ public class SortedSet<T extends Comparable<T>> implements Set<T> {
         return true;    //since it didnt go to false, it will return true once outside of the loop
     }
 
+    //Add a new element to the set
     @Override
     public boolean add(T elem) {
-        if(isFull() && Arrays.binarySearch(elements, 0, size, elem) >= 0)
+        if(isFull() && Arrays.binarySearch(elements, 0, size, elem) >= 0)   //element found, return false to avoid duplicate
             return false;
-        if(isFull())
+        if(isFull())                //set is full, cannot add new element
             throw new FullSetException();
 
-        if(isEmpty()){
+        if(isEmpty()){              //if its empty, add the element and return
             elements[0] = elem;
             size++;
             modified = true;
@@ -71,7 +72,7 @@ public class SortedSet<T extends Comparable<T>> implements Set<T> {
         //Main loop
 
         int tmp = 0;
-
+        // loop to get the index where element is suppose to be place
         for (int i = 0; i < size; i++) {
             if (elem.compareTo(elements[i]) == 0)
                 return false;
@@ -79,13 +80,13 @@ public class SortedSet<T extends Comparable<T>> implements Set<T> {
                 tmp = i+1;
             }
         }
+
+        //move every element by one to the right
         size++;
-        System.out.println(size);
         for (int i = size-1; i > tmp ; i--) {
             elements[i] = elements[i-1];
         }
-        //if(modified == false) throw new TraversalException();
-        elements[tmp] = elem;
+        elements[tmp] = elem;   //element is place properly
         modified = true;
         return true;
     }
@@ -93,9 +94,9 @@ public class SortedSet<T extends Comparable<T>> implements Set<T> {
     @Override
     public boolean remove(T elem) {
         int tmp = Arrays.binarySearch(elements, 0, size, elem);
-        if(tmp >= 0) {
+        if(tmp >= 0) {              //if tmp is smaller than 0, this mean the removed element doesnt exist in the set
             for (int i = tmp; i < size; i++) {
-                    elements[i] = elements[i+1];
+                    elements[i] = elements[i+1];    //move elements to the left
             }
             size--;
             //if(modified == false) throw new TraversalException();
@@ -105,41 +106,53 @@ public class SortedSet<T extends Comparable<T>> implements Set<T> {
         return false;
     }
 
+    //return the size of the set
     @Override
     public int size() {
         return size;
     }
 
+    //return true if the set is empty
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
+    //return true if the set if full
     @Override
     public boolean isFull() {
         return size == elements.length;
     }
 
+    //return the set of element as string
     @Override
     public String toString(){
-        String set = "";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append('{');
+        boolean first = true;
         for (int i = 0; i < size; i++) {
-            if (i < size-1)
-                set += elements[i] + ", ";
+            if (first)
+                first = false;
             else
-                set += elements[i];
+                stringBuilder.append(", ");
+            stringBuilder.append(elements[i]);
         }
-        return "{"+set+"}";
+        stringBuilder.append('}');
+
+        return stringBuilder.toString();
     }
 
-    //
 
+    //return the smallest element of the set
+    //since the set is sorted, we can assume the first element is the smallest
     public T min() {
     if (isEmpty())
         throw new EmptySetException();
     return elements[0];
     }
 
+    //return the largest element of the set
+    //since the set is sorted, we can assume the last element is the largest
     public T max() {
     if (isEmpty())
         throw new EmptySetException();
@@ -154,13 +167,8 @@ public class SortedSet<T extends Comparable<T>> implements Set<T> {
      * @return
      */
 
-    //I assume low and high are integer, otherwise there is a infinity number of element between low and high
+    //get the element between low and high
     public SortedSet<T> subset(T low, T high) {
-        System.out.println("low: " + low);
-        System.out.println("high: " + high);
-
-        System.out.println(this.toString());
-
         if (low.compareTo(high) == 1)
             throw new IllegalArgumentException();
         SortedSet<T> sortedSet = new SortedSet<T>();
@@ -187,21 +195,23 @@ public class SortedSet<T extends Comparable<T>> implements Set<T> {
             }
         }
 
+        //attach the element inside of the limit in the new sorted set
         for (int i = lowIndex; i < highIndex ; i++) {
             sortedSet.add(elements[i]);
         }
-
-        System.out.println(highIndex);
-        System.out.println(lowIndex);
         return sortedSet;
     }
 
+    //reset the pointer to zero
+    //element inside is untouch
+    //modify become false, since its reseted.
     @Override
     public void reset() {
         modified = false;
         front = 0;
     }
 
+    //get the next element of the set according to the pointer of the traversal
     @Override
     public T next() {
         if (!hasNext() || modified)
@@ -210,6 +220,7 @@ public class SortedSet<T extends Comparable<T>> implements Set<T> {
 
     }
 
+    //check if there is an element in front of the pointer of the traversal
     @Override
     public boolean hasNext() {
         return front < size;
